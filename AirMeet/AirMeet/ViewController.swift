f839abac906543afa53e1a99918741f942fd0096
Vsevolond <vsevolond@gmail.com>
2024-04-06 18:49:34 +0300

    PROD: started project

---
+++ AirMeet/AirMeet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AirMeet
+//
+//  Created by Всеволод Донченко on 06.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
